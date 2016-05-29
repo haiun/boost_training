676960ed3f30b4d5276e77051258eca96abda7cf
haiun <gewdox@naver.com>
2016-05-30 02:36:04 +0900

    라이브러리 상대경로 겸 깃헙에서 머지

--- asio_app/Connection.h
+++ asio_app/Connection.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <boost/array.hpp>
 
 class Connection
 {
+	enum { BUFFERSIZE = 512 };
+
+public:
+	void Read();
+	void Send();
+
+	void OnRead(const boost::system::error_code& error, std::size_t byte_transferred);
+	void OnSend();
+
 public:
 	boost::asio::ip::tcp::socket* socket;
 	boost::system::error_code error;
+	boost::array<char, BUFFERSIZE> readBufer;
 };
